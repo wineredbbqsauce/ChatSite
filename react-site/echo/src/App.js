3fa4fa1c19f40cd1c5ccc1dbbc1a6b024f5de5d9
wineredbbqsauce <wineredbbqsauce@gmail.com>
2025-11-14 09:25:36 +0100

    Added Static Chat site

---
+++ react-site/echo/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import FusionChat from './pages/main';
+import './styles/index.css';
+
+function App() {
+  return ( <FusionChat/>);
+}
+
+export default App;
